85603bbd7d334dceb68bf7ac25eeb998aa0a508a
JakubCzech98 <63198539+JakubCzech98@users.noreply.github.com>
2020-10-31 16:14:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import csv
 import time
 from datetime import datetime
 
-
-class ArduinBoardTester:
+class ArduinoBoardTester:
     PINS = (0, 1, 2, 3)
-    board = pyfirmata.Arduino('COM3')
+    board = pyfirmata.Arduino('/dev/cu.usbmodem14201')
     test_iterator = 0
     cols=4
     values = [0.0] * cols
@@ -17,18 +16,16 @@ class ArduinBoardTester:
         for pin in self.PINS:
             self.board.analog[pin].enable_reporting()
         self.read()
-
     def read(self):
         while self.test_iterator < 60:
             if (self.board.analog[0].read() != None and self.board.analog[1].read() != None and self.board.analog[
                 2].read() != None and self.board.analog[3].read() != None):
                 for x1 in range(self.cols):
-                    self.values[x1] += self.board.analog[x1].read() * 5.0 / 1023.0
+                    self.values[x1] += self.board.analog[x1].read() * 5.0
             self.test_iterator += 1
             time.sleep(1)
-            print(self.test_iterator)
+            # print(self.test_iterator)
         self.write_row()
-
     def write_row(self):
         for x1 in range(self.cols):
             self.values[x1] /= 60.0
@@ -38,20 +35,14 @@ class ArduinBoardTester:
         with open(self.filepath, "a", newline='') as f:
             writer = csv.writer(f)
             now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
+            current_time = now.strftime("%m-%d %H:%M:%S")
             writer.writerow([current_time, self.values[0],   self.values[1],   self.values[2],   self.values[3]])
             print([  self.values[0],   self.values[1],   self.values[2],   self.values[3]])
         for x1 in range(self.cols):
             self.values[x1] = 0.0
         self.test_iterator = 0
 
-
-
-
-
-
-
 if __name__ == '__main__':
-    tester = ArduinBoardTester()
+    tester = ArduinoBoardTester()
     while (True):
         tester.read()
